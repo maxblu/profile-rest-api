49db3f5f3ee19db2adff9b352ffaf3546dc56a2b
Maxblu95 <danydelaosa@gmail.com>
2020-10-15 22:49:45 -0400

    Added helloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """ test API view """
+
+    def get(self,request,format=None):
+        """
+        Returns a list of APIView features
+        """
+        
+        an_apiview = [
+            'Uses HTTP methods as funtions (get,post,patch ,put ,delete)',
+            'Is similar to a tradicional djanfo view',
+            'Gives the must controls over app logic',
+            'Is mapped manualy to Urls'
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
+    
+
